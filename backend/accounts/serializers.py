d6ee4b8d0b1f25267283e22bcfec47fd7a5d4049
Student Portal Dev <dev@studentportal.com>
2026-07-27 10:25:23 +0300

    login correction

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,10 +5,23 @@ from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    username = serializers.CharField(required=False, default='')
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'role', 'phone', 'date_of_birth')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'role', 'phone', 'date_of_birth')
+
+    def validate(self, attrs):
+        base = attrs['email'].split('@')[0]
+        username = base
+        counter = 1
+        while User.objects.filter(username=username).exists():
+            username = f"{base}{counter}"
+            counter += 1
+        attrs['username'] = username
+        return attrs
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
@@ -17,5 +30,5 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'role', 'phone', 'date_of_birth')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'role', 'phone', 'date_of_birth')
         read_only_fields = ('id', 'email', 'role')
